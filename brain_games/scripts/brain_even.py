c39c1af51bae06364e9e2062e9085c1b779ab011
a.kholopov <a.kholopov@samolet.ru>
2023-12-21 14:57:28 +0300

    commit7-game-even

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,7 +1,29 @@
 import random
-import sys
-sys.path.append('../brain_games')
 
-from cli.py import welcome_user
+from brain_games.cli import welcome_user
 
-welcome_user()
+def game1(name):
+
+    i = 0 # счетчик правильных ответов
+    print('Answer "yes" if the number is even, otherwise answer "no"')
+    while i < 3:
+        n = random.randint(0, 100)
+        print(f'{"Question:"} {n}')
+        user_answer = input("Your answer: ") # ответ пользователя
+        if user_answer.lower() == "yes" and n%2 == 0:
+            print("Correct!")
+            i += 1
+        elif user_answer.lower() == "no" and n%2 != 0:
+            print("Correct!")
+            i += 1
+        else:
+            print("'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, " + name)
+            break
+    else:
+        print(f'Congratulations, {name}')
+
+name = welcome_user()
+game1(name)
+
+if __name__ == "__main__":
+    main()
