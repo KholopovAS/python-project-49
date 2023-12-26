76c345c7cf8ee69c4c54b5c272ddf53a5da4efcc
a.kholopov <a.kholopov@samolet.ru>
2023-12-26 14:34:50 +0300

    fix brain_progression.py

--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -18,20 +18,25 @@ def generate_progression():
 
 
 def main():
+    score = 0  # Инициализация счетчика
+    print("What number is missing in the progression?")
     while True:
         progression, is_hidden, correct_answer = generate_progression()
+        print("Question:", end=' ')
         print(" ".join(map(str, progression)))
         user_answer = int(input("Your answer: "))
         if user_answer == correct_answer:
             print("Correct!")
-            print(f"Congratulations, {name}!")
-            break
+            score += 1  # Увеличение счетчика
+            if score >= 3:  # Проверка, достигли ли мы трех правильных ответов
+                print(f'Congratulations, {name}!')
+                break
         else:
             print(f"'{user_answer}' is wrong answer ;(. Correct answer was '{correct_answer}'")
             print(f"Let's try again, {name}!")
+            break
 
-
-main()
 
 if __name__ == "__main__":
     main()
+
